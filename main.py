b22938b98b8d490d6ebbadfcd71958e781608c33
Amro Abdelqader <129072457+amro2003-ai@users.noreply.github.com>
2024-11-09 16:22:27 +0300

    main

---
+++ main.py
@@ -0,0 +1,40 @@
+# pip install -r requirements.txt
+# uvicorn main:app --reload
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from model import Model
+
+app = FastAPI()
+
+model = Model()
+
+class FeaturesPackage(BaseModel):
+    cap_diameter : int
+    cap_shape : int
+    gill_attachment : int
+    gill_color : int
+    stem_height : float
+    stem_width : int
+    stem_color : int
+    season : float
+
+
+@app.post('/predict/')
+async def predict(package : FeaturesPackage):
+
+    try:
+        prediction = model.predict(
+            package.cap_diameter,
+            package.cap_shape,
+            package.gill_attachment,
+            package.gill_color,
+            package.stem_height,
+            package.stem_width,
+            package.stem_color,
+            package.season
+        )
+        return {'prediction': prediction}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
